78f1a05810086c0db814be5bf29049e1182085a0
[dyad] <git@dyad.sh>
2026-03-28 22:59:01 +0530

    [dyad] Added the "profileimg" Cloudinary upload preset to the .env file and updated the TypeScript environment definitions. - wrote 2 file(s)

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -5,6 +5,7 @@ interface ImportMetaEnv {
   readonly VITE_CLOUDINARY_UPLOAD_PRESET_PRODUCTS: string;
   readonly VITE_CLOUDINARY_UPLOAD_PRESET_CATEGORIES: string;
   readonly VITE_CLOUDINARY_UPLOAD_PRESET_BANNERS: string;
+  readonly VITE_CLOUDINARY_UPLOAD_PRESET_PROFILE: string;
 }
 
 interface ImportMeta {
